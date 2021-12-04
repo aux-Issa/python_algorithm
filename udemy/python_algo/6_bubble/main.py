58c4f83e52a229a7fa08ebe2a9c58f7463118168
aux-Issa <issa1116184028@gmail.com>
2021-12-04 23:58:51 +0900

    cocktail sort

--- udemy/python_algo/6_bubble/main.py
+++ udemy/python_algo/6_bubble/main.py
@@ -1,17 +1,45 @@
+# from typing import List
+# import random
+#
+# # 12/3 I  couldn't solve
+# def bubble_sort(numbers: List[int]) -> List[int]:
+#     len_numbers = len(numbers)
+#     for i in range(len_numbers):
+#         for j in range(len_numbers - 1 - i):
+#             if numbers[j] > numbers[j + 1]:
+#                 numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j]
+#
+#     return numbers
+#
+#
+# if __name__ == '__main__':
+#     nums = [random.randint(0, 1000) for i in range(10)]
+#     print(bubble_sort(nums))
+
 from typing import List
-import random
 
-# 12/3 I  couldn't solve
-def bubble_sort(numbers: List[int]) -> List[int]:
+def cocktail_sort(numbers: List) -> List[int]:
     len_numbers = len(numbers)
-    for i in range(len_numbers):
-        for j in range(len_numbers - 1 - i):
-            if numbers[j] > numbers[j + 1]:
-                numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j]
+    swapped = True
+    start = 0
+    end = len_numbers - 1
+    while swapped:
+        swapped = False
+        for i in range(start, end):
+            if numbers[i] > numbers[i+1]:
+                numbers[i], numbers[i+1] = numbers[i+1], numbers[i]
+                swapped = True
+            if not swapped:
+                break
 
+        swapped = False
+        end = end - 1
+
+        for i in range(end-1, start-1, -1):
+            if numbers[i] > numbers[i + 1]:
+                numbers[i], numbers[i + 1] = numbers[i + 1], numbers[i]
+                swapped = True
+
+        start = start + 1
+        
     return numbers
-
-
-if __name__ == '__main__':
-    nums = [random.randint(0, 1000) for i in range(10)]
-    print(bubble_sort(nums))
