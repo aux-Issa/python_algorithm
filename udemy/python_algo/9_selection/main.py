00d939d1f332c6a7f949cf5651e4a6cdd08fa63f
aux-Issa <issa1116184028@gmail.com>
2021-12-12 22:49:58 +0900

    a little fix

--- udemy/python_algo/9_selection/main.py
+++ udemy/python_algo/9_selection/main.py
@@ -16,20 +16,3 @@ if __name__ == '__main__':
     print(numbers)
     print(selection_sort(numbers))
 
-
-# from typing import List
-# def selection_sort(numbers: List[int]):
-#     length_num = len(numbers)
-#     for i in range(length_num):
-#         min_index = i
-#         for j in range(i+1, length_num):
-#             if numbers[min_index] > numbers[j]:
-#                 min_index = j
-#         numbers[i], numbers[min_index] = numbers[min_index], numbers[i]
-#     return numbers
-#
-# if __name__ == '__main__':
-#     import random
-#     numbers = [random.randint(0, 1000) for _ in range(10)]
-#     print(numbers)
-#     print(selection_sort(numbers))
