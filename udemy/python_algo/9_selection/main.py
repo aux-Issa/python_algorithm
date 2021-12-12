d74fe50b14788390f5019d9e8a5b527ed9e39d66
aux-Issa <issa1116184028@gmail.com>
2021-12-12 22:49:23 +0900

    try again selection_sort

--- udemy/python_algo/9_selection/main.py
+++ udemy/python_algo/9_selection/main.py
@@ -1,36 +1,35 @@
-# from typing import List
-#
-#
-# def selection_sort(numbers: List[int]) -> List[int]:
-#     for i in range(len(numbers)):
-#         min_idx = i
-#         for j in range(i+1, len(numbers)):
-#             if numbers[min_idx] > numbers[j]:
-#                 min_idx = j
-#
-#         numbers[i], numbers[min_idx] = numbers[min_idx], numbers[i]
-#     return numbers
-#
-#
-# if __name__ == '__main__':
-#     import random
-#     nums = [random.randint(0, 1000) for _ in range(10)]
-#     print(selection_sort(nums))
-
 from typing import List
 
 def selection_sort(numbers: List[int]):
     length_num = len(numbers)
     for i in range(length_num):
-        min_index = i
+        temp_min_index = i
         for j in range(i+1, length_num):
-            if numbers[min_index] > numbers[j]:
-                min_index = j
-        numbers[i], numbers[min_index] = numbers[min_index], numbers[i]
+            if numbers[temp_min_index] > numbers[j]:
+                temp_min_index = j
+        numbers[i], numbers[temp_min_index] = numbers[temp_min_index], numbers[i]
     return numbers
 
 if __name__ == '__main__':
     import random
-    numbers = [random.randint(0, 1000) for _ in range(10)]
+    numbers = [random.randint(0, 1000) for i in range(10)]
     print(numbers)
     print(selection_sort(numbers))
+
+
+# from typing import List
+# def selection_sort(numbers: List[int]):
+#     length_num = len(numbers)
+#     for i in range(length_num):
+#         min_index = i
+#         for j in range(i+1, length_num):
+#             if numbers[min_index] > numbers[j]:
+#                 min_index = j
+#         numbers[i], numbers[min_index] = numbers[min_index], numbers[i]
+#     return numbers
+#
+# if __name__ == '__main__':
+#     import random
+#     numbers = [random.randint(0, 1000) for _ in range(10)]
+#     print(numbers)
+#     print(selection_sort(numbers))
