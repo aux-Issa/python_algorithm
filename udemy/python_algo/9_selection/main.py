df5a4cf6d2c915be3c9823b935a68ad51c61ee69
aux-Issa <issa1116184028@gmail.com>
2021-12-26 16:43:36 +0900

    trimed

--- udemy/python_algo/9_selection/main.py
+++ udemy/python_algo/9_selection/main.py
@@ -1,14 +1,14 @@
 from typing import List
 
-def selection_sort(numbers: List[int]):
-    length_num = len(numbers)
-    for i in range(length_num):
-        temp_min_index = i
-        for j in range(i+1, length_num):
-            if numbers[temp_min_index] > numbers[j]:
-                temp_min_index = j
-        numbers[i], numbers[temp_min_index] = numbers[temp_min_index], numbers[i]
-    return numbers
+# def selection_sort(numbers: List[int]):
+#     length_num = len(numbers)
+#     for i in range(length_num):
+#         temp_min_index = i
+#         for j in range(i+1, length_num):
+#             if numbers[temp_min_index] > numbers[j]:
+#                 temp_min_index = j
+#         numbers[i], numbers[temp_min_index] = numbers[temp_min_index], numbers[i]
+#     return numbers
 
 if __name__ == '__main__':
     import random
