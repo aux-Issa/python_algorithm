93bb062bed6e1a820e00ef30c99c54955fb7cfe1
aux-Issa <issa1116184028@gmail.com>
2022-01-02 21:53:32 +0900

    complete Insetrion sort

--- udemy/python_algo/11_insertion/main.py
+++ udemy/python_algo/11_insertion/main.py
@@ -1,22 +1,37 @@
-from typing import List
+# from typing import List
+#
+#
+# def insertion_sort(numbers: List[int]) -> List[int]:
+#     len_numbers = len(numbers)
+#     for i in range(1, len_numbers):
+#         temp = numbers[i]
+#         j = i - 1
+#
+#         while j >= 0 and numbers[j] > temp:
+#             numbers[j + 1] = numbers[j]
+#             j -= 1
+#
+#         numbers[j + 1] = temp
+#
+#     return numbers
+#
+#
+# if __name__ == '__main__':
+#     import random
+#     nums = [random.randint(0, 1000) for _ in range(10)]
+#     print(insertion_sort(nums))
 
+from typing import List
 
 def insertion_sort(numbers: List[int]) -> List[int]:
     len_numbers = len(numbers)
     for i in range(1, len_numbers):
         temp = numbers[i]
         j = i - 1
-
         while j >= 0 and numbers[j] > temp:
-            numbers[j + 1] = numbers[j]
+            numbers[j+1] = numbers[j]
             j -= 1
-
-        numbers[j + 1] = temp
-
+        numbers[j+1] = tem
     return numbers
 
 
-if __name__ == '__main__':
-    import random
-    nums = [random.randint(0, 1000) for _ in range(10)]
-    print(insertion_sort(nums))
