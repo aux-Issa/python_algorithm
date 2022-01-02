a4e0897e9177c38bf3b23c3342a1d767f3473a40
aux-Issa <issa1116184028@gmail.com>
2022-01-02 22:34:47 +0900

    bucket_sprt

--- udemy/python_algo/12_bucket/main.py
+++ udemy/python_algo/12_bucket/main.py
@@ -19,6 +19,28 @@ def insertion_sort(numbers: List[int]) -> List[int]:
     return numbers
 
 
+# def bucket_sort(numbers: List[int]) -> List[int]:
+#     max_num = max(numbers)
+#     len_numbers = len(numbers)
+#     size = max_num // len_numbers
+#
+#     buckets = [[] for _ in range(size)]
+#     for num in numbers:
+#         i = num // size
+#         if i != size:
+#             buckets[i].append(num)
+#         else:
+#             buckets[size-1].append(num)
+#
+#     for i in range(size):
+#         insertion_sort(buckets[i])
+#
+#     result = []
+#     for i in range(size):
+#         result += buckets[i]
+#
+#     return result
+
 def bucket_sort(numbers: List[int]) -> List[int]:
     max_num = max(numbers)
     len_numbers = len(numbers)
@@ -30,17 +52,15 @@ def bucket_sort(numbers: List[int]) -> List[int]:
         if i != size:
             buckets[i].append(num)
         else:
+            # 割り切れる場合はbucketの一番最後に追加する
             buckets[size-1].append(num)
-
     for i in range(size):
+        print(buckets[i])
         insertion_sort(buckets[i])
-
     result = []
     for i in range(size):
         result += buckets[i]
-
     return result
-
 
 if __name__ == '__main__':
     import random
