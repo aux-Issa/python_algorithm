db359aabd99a084c328abac8c24e346b4ac48501
aux-Issa <issa1116184028@gmail.com>
2022-01-03 16:48:29 +0900

    complete quick_sort

--- udemy/python_algo/16_quick/main.py
+++ udemy/python_algo/16_quick/main.py
@@ -1,7 +1,27 @@
 from typing import List
 
 
-def partition(numbers: List[int], low: int, high: int) -> int:
+# def partition(numbers: List[int], low: int, high: int) -> int:
+#     i = low - 1
+#     pivot = numbers[high]
+#     for j in range(low, high):
+#         if numbers[j] <= pivot:
+#             i += 1
+#             numbers[i], numbers[j] = numbers[j], numbers[i]
+#     numbers[i+1], numbers[high] = numbers[high], numbers[i+1]
+#     return i+1
+#
+#
+# def quick_sort(numbers: List[int]) -> List[int]:
+#     def _quick_sort(numbers: List[int], low: int, high: int) -> None:
+#         if low < high:
+#             partition_index = partition(numbers, low, high)
+#             _quick_sort(numbers, low, partition_index-1)
+#             _quick_sort(numbers, partition_index+1, high)
+#
+#     _quick_sort(numbers, 0, len(numbers)-1)
+#      return numbers
+def partition(numbers: List[int], high: int, low: int) -> int:
     i = low - 1
     pivot = numbers[high]
     for j in range(low, high):
@@ -11,17 +31,14 @@ def partition(numbers: List[int], low: int, high: int) -> int:
     numbers[i+1], numbers[high] = numbers[high], numbers[i+1]
     return i+1
 
-
 def quick_sort(numbers: List[int]) -> List[int]:
     def _quick_sort(numbers: List[int], low: int, high: int) -> None:
         if low < high:
             partition_index = partition(numbers, low, high)
             _quick_sort(numbers, low, partition_index-1)
             _quick_sort(numbers, partition_index+1, high)
-
     _quick_sort(numbers, 0, len(numbers)-1)
     return numbers
-
 
 if __name__ == '__main__':
     import random
