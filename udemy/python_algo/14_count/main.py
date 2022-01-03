b3d079abfc503b7eb1378b3a2eb9e944a6f5f6f2
aux-Issa <issa1116184028@gmail.com>
2022-01-03 15:31:01 +0900

    complete counting_sort

--- udemy/python_algo/14_count/main.py
+++ udemy/python_algo/14_count/main.py
@@ -1,11 +1,34 @@
-from typing import List
-
+# from typing import List
+#
+#
+# def counting_sort(numbers: List[int]) -> List[int]:
+#     max_num = max(numbers)
+#     counts = [0] * (max_num + 1)
+#     result = [0] * len(numbers)
+#
+#     for num in numbers:
+#         counts[num] += 1
+#
+#     for i in range(1, len(counts)):
+#         counts[i] += counts[i-1]
+#
+#     i = len(numbers) - 1
+#     while i >= 0:
+#         index = numbers[i]
+#         result[counts[index]-1] = numbers[i]
+#         counts[index] -= 1
+#         i -= 1
+#
+#     return result
+from typing import  List
 
 def counting_sort(numbers: List[int]) -> List[int]:
     max_num = max(numbers)
-    counts = [0] * (max_num + 1)
-    result = [0] * len(numbers)
-
+    # numbers内の特定の数字の登場をカウントする配列
+    counts = [0]*(max_num + 1)
+    # sort後の結果を示す配列
+    results = [0]*len(numbers)
+    # numbersの各要素をカウントし，counterに格納
     for num in numbers:
         counts[num] += 1
 
@@ -15,14 +38,12 @@ def counting_sort(numbers: List[int]) -> List[int]:
     i = len(numbers) - 1
     while i >= 0:
         index = numbers[i]
-        result[counts[index]-1] = numbers[i]
+        results[counts[index]-1] = numbers[i]
         counts[index] -= 1
         i -= 1
+    return results
 
-    return result
-
-
-if __name__ == '__main__':
-    import random
-    nums = [random.randint(0, 1000) for _ in range(10)]
-    print(counting_sort(nums))
+# if __name__ == '__main__':
+#     import random
+#     nums = [random.randint(0, 1000) for _ in range(10)]
+#     print(counting_sort(nums))
