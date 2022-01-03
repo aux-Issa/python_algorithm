c973ca6d296697b1fea96ab8c1540ba856a5464f
aux-Issa <issa1116184028@gmail.com>
2022-01-03 16:59:57 +0900

    debug

--- udemy/python_algo/16_quick/main.py
+++ udemy/python_algo/16_quick/main.py
@@ -32,15 +32,19 @@ def partition(numbers: List[int], high: int, low: int) -> int:
     return i+1
 
 def quick_sort(numbers: List[int]) -> List[int]:
+
     def _quick_sort(numbers: List[int], low: int, high: int) -> None:
+        
         if low < high:
             partition_index = partition(numbers, low, high)
-            _quick_sort(numbers, low, partition_index-1)
-            _quick_sort(numbers, partition_index+1, high)
+            _quick_sort(numbers, low, partition_index - 1)
+            _quick_sort(numbers, partition_index + 1, high)
+
     _quick_sort(numbers, 0, len(numbers)-1)
     return numbers
 
 if __name__ == '__main__':
     import random
     nums = [random.randint(0, 1000) for _ in range(10)]
+    print(nums)
     print(quick_sort(nums))
